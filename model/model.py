fedcbd1c05b269982858ad20947ee0dc55509d0f
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-11-21 00:44:06 +0800

    add skeleton for random forest

--- model/model.py
+++ model/model.py
@@ -3,6 +3,11 @@ Use random forest
 """
 from sklearn.model_selection import train_test_split
 
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+
+# create random forest model
+random_forest_classifier = RandomForestClassifier()
 
 
 
